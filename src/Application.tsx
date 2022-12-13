53238bee0b244c74cc62ebc0c9f47078a33df4ed
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-13 09:28:18 +0200

    reducer.ts

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { nanoid } from "nanoid";
 
 import { Main } from "./partials/Main";
 import { Editor } from "./partials/Editor";
 
 import type { Note } from "./types";
+import { initialState, reducer } from "./reducer";
 
 export function Application() {
-  const [open, setOpen] = React.useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const [notes, setNotes] = React.useState<Note[]>([]);
   const [currentNoteId, setCurrentNoteId] = React.useState(
@@ -19,16 +20,18 @@ export function Application() {
       {notes.length > 0 ? (
         <React.Fragment>
           <Main
-            open={open}
-            openEditor={openEditor}
+            open={state.editorOpen}
+            openEditor={() => dispatch({ type: "openEditor", payload: true })}
             notes={notes}
             addNote={addNote}
             setCurrentNoteId={setCurrentNoteId}
           />
 
           <Editor
-            open={open}
-            closeEditor={closeEditor}
+            open={state.editorOpen}
+            closeEditor={() =>
+              dispatch({ type: "closeEditor", payload: false })
+            }
             currentNote={findCurrentNote()}
             setNotes={setNotes}
           />
@@ -42,14 +45,6 @@ export function Application() {
     </div>
   );
 
-  function closeEditor() {
-    setOpen(false);
-  }
-
-  function openEditor() {
-    setOpen(true);
-  }
-
   function addNote() {
     const note: Note = {
       title: "Enter your title",
@@ -62,7 +57,7 @@ export function Application() {
 
     setNotes((prevNotes) => [note, ...prevNotes]);
     setCurrentNoteId(note.id);
-    openEditor();
+    dispatch({ type: "openEditor", payload: true });
   }
 
   function findCurrentNote() {
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,3 +1,25 @@
-export function reducer() {
+import type { Note } from "./types"
 
+type ActionType =
+    | { type: 'openEditor'; payload: boolean }
+    | { type: 'closeEditor'; payload: boolean }
+
+export const initialState = {
+    editorOpen: false,
+    currentNoteId: 0,
+    notes: [] as Note[]
+}
+
+export function reducer(state: typeof initialState, action: ActionType) {
+    switch (action.type) {
+        case "openEditor":
+        case "closeEditor":
+            return {
+                ...state,
+                editorOpen: action.payload
+            }
+
+        default:
+            throw new Error('unknown action');
+    }
 }
